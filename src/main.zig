c797267adea09fdcb37fd68765d424a33dab86e3
Dongdong Yang <donge@donge.org>
2026-06-13 10:01:41 +0800

    release: add back linux-arm64, bump to v1.0.1

--- src/main.zig
+++ src/main.zig
@@ -508,7 +508,7 @@ fn runCommand(init: std.process.Init, allocator: std.mem.Allocator, args: *std.p
         cfg.mat_views = mat_views;
         try compactor.run(allocator, init.io, cfg);
     } else if (std.mem.eql(u8, command, "--version") or std.mem.eql(u8, command, "-v")) {
-        try writeOut(init.io, "zighouse v1.0.0\n");
+        try writeOut(init.io, "zighouse v1.0.1\n");
     } else if (std.mem.eql(u8, command, "help") or std.mem.eql(u8, command, "--help") or std.mem.eql(u8, command, "-h")) {
         try printUsage(init.io);
     } else {
